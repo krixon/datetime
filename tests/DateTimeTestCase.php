df574b1c06d760875e179ffaa9f84b0696ca28cb
karl.rixon <karl.rixon@maruedr.com>
2017-03-30 10:17:19 +0100

    Ensure all tests run in the UTC timezone

--- tests/DateTimeTestCase.php
+++ tests/DateTimeTestCase.php
@@ -6,6 +6,14 @@ use Krixon\DateTime\DateTime;
 
 class DateTimeTestCase extends \PHPUnit_Framework_TestCase
 {
+    public static function setUpBeforeClass()
+    {
+        parent::setUpBeforeClass();
+
+        date_default_timezone_set('UTC');
+    }
+
+
     protected static function assertSameDate(string $expected, DateTime $date)
     {
         $expected = DateTime::create($expected);
